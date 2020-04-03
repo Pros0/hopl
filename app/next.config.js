05a33a264506bdfcf25239828b8d8e7c2b850e68
ccromnow <christiancromnow@Christians-MacBook-Pro.local>
2020-04-03 23:48:47 +0200

    adding webpack alias config and init component for search page

---
+++ app/next.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias["components"] = path.join(
+      __dirname,
+      "components"
+    );
+    return config;
+  }
+}

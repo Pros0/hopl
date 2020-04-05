7ce43d035f112c7f300636ced096eb3814934e0f
Adam Ramberg <adam@mambojambostudios.com>
2020-04-05 12:55:07 +0200

    Fix logo on mobile

--- app/components/Logo/index.js
+++ app/components/Logo/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  width: 100px;
+`;
+
 const Img = styled.img`
-  max-width: 100px;
+  display: block;
+  width: 100%;
   height: auto;
 `;
 
-const Logo = () => <Img src="/hopl-logo-square.png" alt="Hopl" />;
+const Logo = () => (
+  <Wrapper>
+    <Img src="/hopl-logo-square.png" alt="Hopl" />
+  </Wrapper>
+);
 
 export default Logo;

1b1fbdd71e8bc6c2079c61d52c66e54d29609675
Svanis <christopher.svanefalk@gmail.com>
2020-04-05 10:02:18 +0200

    lint it up

--- app/components/Menu/listItems.js
+++ app/components/Menu/listItems.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import SearchIcon from '@material-ui/icons/Search';
-import AssignmentIcon from '@material-ui/icons/Assignment';
 import SettingsIcon from '@material-ui/icons/Settings';
 import NextLink from 'next/link';
 

f56efc062a5e46056261f5f4ac6707834f3909f8
Adam Ramberg <adam@mambojambostudios.com>
2020-04-05 12:57:32 +0200

    Increase header bottom margin in login

--- app/components/common/NotLoggedIn/styles.js
+++ app/components/common/NotLoggedIn/styles.js
@@ -17,6 +17,7 @@ export const PageWrapper = styled.div`
 export const LogoWrapper = styled.div`
   width: 100%;
   display: flex;
+  margin-bottom: 2rem;
 `;
 
 export const ContentWrapper = styled.div`

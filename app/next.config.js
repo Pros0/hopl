c6b0f234f1cddc96002a40ed31492e644a06fa2d
Svanis <christopher.svanefalk@gmail.com>
2020-04-05 10:05:28 +0200

    lint a bit more

--- app/next.config.js
+++ app/next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   webpack: (config) => {
-    config.resolve.alias['components'] = path.join(__dirname, 'components');
+    config.resolve.alias.components = path.join(__dirname, 'components');
     return config;
   },
   env: {
